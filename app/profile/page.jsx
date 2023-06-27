f25cec611e6d00a53237686e4297c4474571632f
paryuyu <107971288+paryuyu@users.noreply.github.com>
2023-06-27 16:26:32 +0900

    경고창 추가

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -16,7 +16,7 @@ const MyProfile = () => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
-
+      console.log(data,'data')
       setMyPosts(data);
     };
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -15,9 +15,9 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const handleProfileClick = () => {
     console.log(post);
 
-    if (post.creator._id === session?.user.id) return router.push("/profile");
-    
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+      if (post.creator._id === session?.user.id) return router.push("/profile");
+    alert("본인의 프로필만 확인가능합니다.")
+    // router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
 
   const handleCopy = () => {
